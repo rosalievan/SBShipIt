335becc4a9e12cf17a9037e4b6e723a6d3ddffe1
rosalievan <r.vanonzenoort98@gmail.com>
2022-09-30 12:29:45 +0100

    first commit

--- ShipIt/Models/ApiModels/OutboundOrderRequestModel.cs
+++ ShipIt/Models/ApiModels/OutboundOrderRequestModel.cs
@@ -11,11 +11,19 @@ namespace ShipIt.Models.ApiModels
         public int WarehouseId { get; set; }
         public IEnumerable<OrderLine> OrderLines { get; set; }
 
+        // public int orderweight = 0;
+
         public override String ToString()
         {
+            
+            // foreach (OrderLine orderline in OrderLines)
+            // {
+            //     orderweight += orderline.Weight;
+            // }
             return new StringBuilder()
                 .AppendFormat("warehouseId: {0}, ", WarehouseId)
                 .AppendFormat("orderLines: {0}", OrderLines)
+                // .AppendFormat("numberOfTrucks: {0}", orderweight/200)
                 .ToString();
         }
     }
